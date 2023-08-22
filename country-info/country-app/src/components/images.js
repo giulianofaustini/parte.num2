cfe8f0f4567c611e3e0d8feb2878683b1f14834e
giulianofaustini <139208335+giulianofaustini@users.noreply.github.com>
2023-08-22 09:19:19 +0300

    part 2

---
+++ country-info/country-app/src/components/images.js
@@ -0,0 +1,15 @@
+const images = {
+    "01d": "http://openweathermap.org/img/wn/01d@2x.png",
+    "02d": "http://openweathermap.org/img/wn/02d@2x.png",
+    "03d": "http://openweathermap.org/img/wn/03d@2x.png",
+    "04d": "http://openweathermap.org/img/wn/04d@2x.png",
+    "09d": "http://openweathermap.org/img/wn/09d@2x.png",
+    "10d": "http://openweathermap.org/img/wn/10d@2x.png",
+    "11d": "http://openweathermap.org/img/wn/11d@2x.png",
+    "13d": "http://openweathermap.org/img/wn/13d@2x.png",
+    "50d": "http://openweathermap.org/img/wn/50d@2x.png",
+    
+  };
+  
+  export default images;
+  
